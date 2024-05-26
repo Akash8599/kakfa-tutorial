79d5b383a283582201d4848e29da46ff6c221e58
Lenovo <akashkamble8599@gmail.com>
2024-05-26 17:56:02 +0530

    kafka producer tutorial

---
+++ order-service/src/main/java/com/orderService/controller/KafkaProducerController.java
@@ -0,0 +1,24 @@
+package com.orderService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaProducerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping("/send/{message}")
+    public ResponseEntity<String> sendMessage(@PathVariable String message){
+        kafkaTemplate.send("akash-topic", message);
+
+        return new ResponseEntity<>("Successful", HttpStatus.OK);
+    }
+}
